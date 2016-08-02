6c7385c77b8d6731c867b3d5d32bc38e072cd026
박선희(SunHee Park)/Platform Architecture Lab/SKTECH <sunny79@sk.com>
2016-08-02 13:51:19 +0900

    modify controller

--- src/main/java/com/sktechx/palab/logx/web/StatisticsController.java
+++ src/main/java/com/sktechx/palab/logx/web/StatisticsController.java
@@ -14,8 +14,8 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Created by sunny on 2016. 7. 7..
@@ -33,24 +33,25 @@ public class StatisticsController {
     StatisticsExcelExportService statisticsExcelExportService;
 
     //period = {monthly, daily}
-    @RequestMapping(value="{period}/pv", method= RequestMethod.GET)
-    public void getExcelForPV(@PathVariable String period,@RequestParam String service,
+    @RequestMapping(value="{period}/{pvOrUv}", method= RequestMethod.GET)
+    public void getStatisticsExcel(@PathVariable String period, @PathVariable String pvOrUv,
+                                   @RequestParam String service,
                                    @RequestParam(required = false) String option1,
                                    @RequestParam(required=false) String option2,
                                    @RequestParam String startDate, @RequestParam String endDate, HttpServletResponse response) throws IOException {
 
 
+        logger.debug("pvOrUv : {} ", pvOrUv);
         logger.debug("service : {} ", service);
         logger.debug("option1 : {} option2: {}", option1, option2);
         logger.debug("startDate : {} endDate: {}", startDate, endDate);
 
         enumRCType rcType = enumRCType.valueOf(period);
 
-        XSSFWorkbook workbook = statisticsExcelExportService.exportExcel(service, option1, option2, rcType, startDate, endDate, true);
+        XSSFWorkbook workbook = statisticsExcelExportService.exportExcel(service, option1, option2, rcType, startDate, endDate, pvOrUv.equals("pv"));
 
-        Calendar today = Calendar.getInstance();
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-        String date = sdf.format(today.getTime());
+        //오늘 일자
+        String date = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"));
 
         String excelFileName = "stat_pv" + (StringUtils.isEmpty(service) ? "" : "_" + service)
                 + (StringUtils.isEmpty(option1) ? "" : "_" + option1)
