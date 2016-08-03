469260babb7bd8fe3bdb21b61bb56646b86a78f0
박선희(SunHee Park)/Platform Architecture Lab/SKTECH <sunny79@sk.com>
2016-08-03 15:56:05 +0900

    Montly 테스트 데이터 추가를 위해 testMontlyPV를 추가

--- src/main/java/com/sktechx/palab/logx/service/ScheduledTasks.java
+++ src/main/java/com/sktechx/palab/logx/service/ScheduledTasks.java
@@ -38,8 +38,8 @@ public class ScheduledTasks {
 
     //매일 그날의 request call 수를 저장한다
     //매일 0시 5분에 전날 request call를 조회 및 저장
-    //@Scheduled(cron="0 5 12 1/1 * *")
-    @Scheduled(cron="0/30 * * * * *")
+    @Scheduled(cron="0 5 12 1/1 * *")
+    //@Scheduled(cron="0/30 * * * * *")
     public void savecDailyPVUV() throws ParseException {
 
         Calendar cal = Calendar.getInstance();
@@ -58,7 +58,7 @@ public class ScheduledTasks {
 
         try {
 
-            esService.generatePV(enumRCType.daily, date1, date2);
+            esService.generateSvcPV(enumRCType.daily, date1, date2);
             esService.generateSvcOption1PV(enumOptionType.APP, enumRCType.daily, date1, date2);
             esService.generateSvcOption1PV(enumOptionType.API, enumRCType.daily, date1, date2);
             esService.generateSvcOption1PV(enumOptionType.ERROR, enumRCType.daily, date1, date2);
@@ -83,6 +83,51 @@ public class ScheduledTasks {
 
     }
 
+    @Scheduled(cron="0/3 * * * * *")
+    public void testMonthlyPV() throws IOException, ParseException {
+
+        logger.debug("=========================");
+        logger.debug("start every monthly!!");
+        logger.debug("=========================");
+
+        LocalDate start = LocalDate.of(2016, 5, 1);
+
+        //LocalDate tmp = LocalDate.now();
+
+        //LocalDate start = LocalDate.of(tmp.getYear(), tmp.getMonth(), 1);
+
+        LocalDate end = start.plusMonths(5);
+
+        for (; start.isBefore(end); start = start.plusMonths(1) ) {
+
+            String date1 = start.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+            String date2 = start.plusMonths(1).format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+
+            logger.debug("date1 : {} ------------ date2 : {}", date1, date2);
+
+            esService.generateSvcPV(enumRCType.monthly, date1, date2);
+            esService.generateSvcOption1PV(enumOptionType.API, enumRCType.monthly, date1, date1);
+            esService.generateSvcOption1PV(enumOptionType.APP, enumRCType.monthly, date1, date1);
+            esService.generateSvcOption1PV(enumOptionType.ERROR, enumRCType.monthly, date1, date1);
+
+            esService.generateSvcOptionERROR(enumOptionType.ERROR_API, enumRCType.monthly, date1, date2);
+            esService.generateSvcOptionERROR(enumOptionType.ERROR_APP, enumRCType.monthly, date1, date2);
+
+            esService.generateSvcOption2PV(enumOptionType.APP_API, enumRCType.monthly, date1, date2);
+            esService.generateSvcOption2PV(enumOptionType.API_APP, enumRCType.monthly, date1, date2);
+            esService.generateSvcOption2PV(enumOptionType.ERROR_API, enumRCType.monthly, date1, date2);
+            esService.generateSvcOption2PV(enumOptionType.ERROR_APP, enumRCType.monthly, date1, date2);
+
+            esUVService.generateSVCUV(enumRCType.monthly, date1, date2);
+            esUVService.generateSvcOption1UV(enumOptionType.API, enumRCType.monthly, date1, date2);
+            esUVService.generateSvcOption1UV(enumOptionType.APP, enumRCType.monthly, date1, date2);
+            esUVService.generateSvcOption2UV(enumOptionType.APP_API, enumRCType.monthly, date1, date2);
+            esUVService.generateSvcOption2UV(enumOptionType.API_APP, enumRCType.monthly, date1, date2);
+
+        }
+    }
+
+
     //매월 1일 0시 5분 마다
     @Scheduled(cron="0 5 0 1 1/1 ?")
     //@Scheduled(cron="0/3 * * * * *")
@@ -104,14 +149,24 @@ public class ScheduledTasks {
         String date2 = end.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
 
         esService.generateSvcPV(enumRCType.monthly, date1, date2);
-        esService.generateSvcOption1PV(enumOptionType.API       ,enumRCType.monthly, date1, date1);
-        esService.generateSvcOption1PV(enumOptionType.APP       ,enumRCType.monthly, date1, date1);
-        esService.generateSvcOption1PV(enumOptionType.ERROR     ,enumRCType.monthly, date1, date1);
+        esService.generateSvcOption1PV(enumOptionType.API, enumRCType.monthly, date1, date1);
+        esService.generateSvcOption1PV(enumOptionType.APP, enumRCType.monthly, date1, date1);
+        esService.generateSvcOption1PV(enumOptionType.ERROR, enumRCType.monthly, date1, date1);
 
-        esService.generateSvcOption2PV(enumOptionType.APP_API   ,enumRCType.monthly, date1, date2);
-        esService.generateSvcOption2PV(enumOptionType.API_APP   ,enumRCType.monthly, date1, date2);
-        esService.generateSvcOption2PV(enumOptionType.ERROR_API ,enumRCType.monthly, date1, date2);
-        esService.generateSvcOption2PV(enumOptionType.ERROR_APP ,enumRCType.monthly, date1, date2);
+        esService.generateSvcOption2PV(enumOptionType.APP_API, enumRCType.monthly, date1, date2);
+        esService.generateSvcOption2PV(enumOptionType.API_APP, enumRCType.monthly, date1, date2);
+        esService.generateSvcOption2PV(enumOptionType.ERROR_API, enumRCType.monthly, date1, date2);
+        esService.generateSvcOption2PV(enumOptionType.ERROR_APP, enumRCType.monthly, date1, date2);
+        esService.generateSvcOptionERROR(enumOptionType.ERROR_API, enumRCType.monthly, date1, date2);
+        esService.generateSvcOptionERROR(enumOptionType.ERROR_APP, enumRCType.monthly, date1, date2);
+
+        esUVService.generateSVCUV(enumRCType.monthly, date1, date2);
+        esUVService.generateSvcOption1UV(enumOptionType.API, enumRCType.monthly, date1, date2);
+        esUVService.generateSvcOption1UV(enumOptionType.APP, enumRCType.monthly, date1, date2);
+        esUVService.generateSvcOption2UV(enumOptionType.APP_API, enumRCType.monthly, date1, date2);
+        esUVService.generateSvcOption2UV(enumOptionType.API_APP, enumRCType.monthly, date1, date2);
+
+
 
     }
 
