9039c23ddd92d6672b223d680775769ae95ee094
Sunny <sunny79@sk.com>
2016-08-24 17:56:49 +0900

    enable shutdown endpoint

--- src/main/resources/schema-prod.sql
+++ src/main/resources/schema-prod.sql
@@ -1,10 +1,10 @@
 CREATE OR REPLACE VIEW `logx`.`view_svc` AS
-  select tyk_svc_id as svc_id, name from `apixgwdb`.`asset` order by id
+  select tyk_svc_id as svc_id, name from `apigwdb`.`asset` order by id
 ;
 
 CREATE OR REPLACE VIEW `logx`.`view_app` AS
   select b.app_key, a.id as app_id, a.name
-  from `apixgwdb`.`application` as a , `apix`.`appkey` as b where a.appkey_id = b.id
+  from `apigwdb`.`application` as a , `apigwdb`.`appkey` as b where a.appkey_id = b.id
   order by a.id;
 ;
 
